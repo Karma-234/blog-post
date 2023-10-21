33973b662b99001f2d54e5dfc5fddbbd67f91bd6
Karma-234 <busari1kamal@gmail.com>
2023-10-21 07:53:52 -0700

    Added cors handler

--- main.go
+++ main.go
@@ -1,5 +1,39 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
 func main() {
+	godotenv.Load(".env")
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("No Port found")
+	}
+	router := chi.NewRouter()
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https//*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + port,
+	}
+	fmt.Printf("Started server on port %v\n", port)
+	startError := srv.ListenAndServe()
+	if startError != nil {
+		log.Fatal(startError)
+	}
 
 }

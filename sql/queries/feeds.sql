ac2c5e02ac46210e6c16d5b7316298b0bc28ed2b
Karma-234 <busari1kamal@gmail.com>
2023-10-31 17:45:52 -0700

    Added query for fecthing feeds

--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -5,3 +5,15 @@ RETURNING *;
 
 -- name: GetAllFeeds :many
 SELECT * FROM feeds;
+
+-- name: GetNextToFetch :one
+
+SELECT * FROM feeds 
+ORDER BY last_fetched_at ASC NULLS FIRST LIMIT 1;
+
+-- name: MarkFeedAsfetched :one
+UPDATE feeds 
+SET last_fetched_at = NOW(),
+updated_at = NOW() 
+WHERE id = $1
+RETURNING *;
